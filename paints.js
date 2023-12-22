41ec835375473526ad98c383a3527f5888c121a7
subash1310 <150768357+subash1310@users.noreply.github.com>
2023-12-22 15:26:42 +0530

    Add files via upload

--- paints.js
+++ paints.js
@@ -4,14 +4,37 @@ function Paint(){
     return(
         <div>
             <p>paint designs</p>
-            <img src={Dot} className="paint0"></img>
+            <img src={Dot} className="paint0"/>
             <a href="Plans">plans partition</a>
             <button type="button" class="btn btn-success">appear</button>
             <form>
         {/* <label> tag defines a label for many form elements */}
         <label for="fname">Consle login:</label>
         <br />
+        <input type="password"></input>
         </form>
+        <table class="table  table-hover table-borderless">
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Age</th>
+        <th>Email</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr class="table-success"> 
+        <td>Ari</td>
+        <td>25</td>
+        <td>ari@example.com</td>
+      </tr>
+      <tr>
+        <td>Ajith</td>
+        <td>30</td>
+        <td>ajith@example.com</td>
+      </tr>
+   
+    </tbody>
+  </table>
         </div>
     )
 }
